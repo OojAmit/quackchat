f531998407c9cb211ef06348dd58b480ca15c096
OojAmit <oojamit@gmail.com>
2018-01-04 00:03:41 +0530

    Basic Express server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+const express = require('express');
+
+const port = 80;
+const ip = '45.44.8.66';
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('Ooj is awesome!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+})
