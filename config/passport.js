9d44c1d5c585368c7cf9612c8ae2aafb298d682d
OojAmit <oojamit@gmail.com>
2018-03-20 13:08:00 +0530

    Quack Chat => QuackChat. Removed unnecessary console.log. New Logo

--- config/passport.js
+++ config/passport.js
@@ -16,7 +16,6 @@ passport.use(
   }, (accessToken, refreshToken, profile, done) => {
     User.findOne({googleID: profile.id}).then(currentUser => {
       if (currentUser) {
-        console.log('User already in the DB');
         done(null, currentUser)
       } else {
         new User({
